b2c472a22617f5e952d85c6b5563f875c4a5212e
bsilakaymak <basaksilakaymak@gmail.com>
2020-07-10 22:03:52 +0200

    sw example2

--- week0/service-worker.js
+++ week0/service-worker.js
@@ -2,7 +2,12 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches
       .open("cache1")
-      .then((cache) => cache.addAll(["index.html", "index.js", "style.css"]))
+      .then((cache) => {
+        console.log("caching");
+        cache.addAll(["index.html", "index.js", "style.css"]);
+      })
       .then(() => self.skipWaiting())
   );
 });
+
+self.addEventListener("activate", () => console.log("activated"));
